1685e3fc337eed6c4428875ed5a99e5c85ce57dc
Davinson <davinson.tablinum@gmail.com>
2024-02-18 17:03:12 -0500

    Problem resolved, code working correctly.

--- python_repos.py
+++ python_repos.py
@@ -5,7 +5,7 @@ import requests
 url = "https://api.github.com/search/repositories"
 url += "?q=language:python+sort:stars+stars:>10000"
 
-headers = {"Accept": "?q=language:python+sort:stars+stars:>10000"}
+headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
